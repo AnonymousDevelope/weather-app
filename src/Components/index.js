2e0751ca6e7cb6be46ab2d6b5292fc5ad55de439
Anonymous Developer <you@example.com>
2023-08-23 10:34:24 +0500

    remove exucess files and codes

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,4 @@
 export {default as Header} from './header-view/header'
 export {default as Main} from './main-view/main'
-export {default as Footer} from './footer-view/footer'
 export  {default as WeatherDaily} from './weather-card-daily/weatherDaily'
 export {default as WeatherHourly} from './weather-card-hourly/weatherHourly'
